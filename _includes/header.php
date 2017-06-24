50d6898c02b893cce556a34778566310920dfc5f
Josh S <josh.sanger@geekpower.ca>
2017-06-24 15:56:28 -0400

    Added in the font for spectral.

--- _includes/header.php
+++ _includes/header.php
@@ -4,8 +4,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1, maximum-scale=1">
-        <title>ENTER TITLE</title>
+        <title>Pun Generator for Shopify</title>
         <script src="<?php echo BASE_URL;?>/_assets/js/lib/modernizr.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Spectral:300,400,500,600,700" rel="stylesheet">
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
         <link rel="stylesheet" href="<?php echo BASE_URL;?>/_assets/css/master.min.css">
     </head>
